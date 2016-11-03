26c4eceb1722b90ad097665252cb3b6366885ef5
kamil <kammmilkos@gmail.com>
2016-11-03 19:30:22 +0100

    init commit

---
+++ main.cpp
@@ -0,0 +1,17 @@
+/*
+ * Data-Driven Modeling sim engine
+ * main.cpp
+ *
+ *  Created on: 03.11.2016
+ */
+
+#include <iostream>
+
+int main(int argc, char* argv[]) {
+
+	std::cout<<"I am doing something!"<<std::endl;
+	return 0;
+}
+
+
+
